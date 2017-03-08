d27bd8c060ded23abfdfc04b0f8e9033866cc25a
Bruno Goncalves <bgoncalv@redhat.com>
2017-03-08 09:41:36 +0000

    fixed some mis-spelled words and code style

--- smoke.py
+++ smoke.py
@@ -8,11 +8,14 @@ from avocado import main
 from avocado import Test
 
 class BaseRuntimeRunCmd(object):
+    """
+    Class to execute a command to a docker container
+    """
     docker_container = None
 
     def _run_cmd(self, cmd):
         try:
-            test_output = subprocess.check_output(cmd, stderr = subprocess.STDOUT, shell = True)
+            test_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
         except subprocess.CalledProcessError as e:
             return e.returncode, e.output
         return 0, test_output
@@ -21,31 +24,27 @@ class BaseRuntimeRunCmd(object):
     def run_docker_cmd(self, cmd, rm_container=True):
         """
         Execute a docker command. By default it will create a new container
-        Once the command finishes the contaier is removed
+        Once the command finishes the container is removed
         """
         cmdline = 'docker run --rm base-runtime-smoke bash -c "%s"' % cmd
+
         if self.docker_container or not rm_container:
-            #Want to execute command and leave contanier running
+            #Want to execute command and leave container running
             if not self.docker_container:
-                self.docker_container = self._run_br_contanier_background()
+                self.docker_container = self._run_br_container_background()
                 if not self.docker_container:
                     return 1, "FAIL: run_docker_cmd() - Could not start container"
             cmdline = 'docker exec %s bash -c "%s"' % (self.docker_container, cmd)
 
         test_ret, test_output = self._run_cmd(cmdline)
-        if test_ret !=0:
-            #if self.docker_container:
-                #Command failed, remove container - Not sure if this is ideal
-                #self._rm_br_container(self.docker_container)
-                #self.docker_container = None
-            return test_ret, test_output
 
         if self.docker_container and rm_container:
             self._rm_br_container(self.docker_container)
             self.docker_container = None
-        return 0, test_output
 
-    def _run_br_contanier_background(self):
+        return test_ret, test_output
+
+    def _run_br_container_background(self):
         """
         Start new container and keep it running on background
         Return the new container ID
@@ -53,14 +52,16 @@ class BaseRuntimeRunCmd(object):
         #Hack to leave container running: http://stackoverflow.com/questions/30209776/docker-container-will-automatically-stop-after-docker-run-d
         cmdline = "docker run -d base-runtime-smoke bash -c 'tail -f /dev/null'"
         test_ret, test_output = self._run_cmd(cmdline)
-        if test_ret !=0:
+        if test_ret != 0:
             return test_ret, test_output
+
         test_output = test_output.strip()
         if not test_output:
             return None
+
         container_ids = test_output.split('\n')
         if len(container_ids) > 1:
-            print "FAIL: _run_br_contanier_background() - Found more containers ID then expected"
+            print "FAIL: _run_br_container_background() - Found more containers ID then expected"
             return None
         return container_ids[0]
 
@@ -69,13 +70,12 @@ class BaseRuntimeRunCmd(object):
         """
         cmdline = "docker stop %s" % container_id
         test_ret, test_output = self._run_cmd(cmdline)
-        if test_ret !=0:
-            return e.returncode
+        if test_ret != 0:
+            return test_ret
+
         cmdline = "docker rm %s" % container_id
         test_ret, test_output = self._run_cmd(cmdline)
-        if test_ret !=0:
-            return test_ret
-        return 0
+        return test_ret
 
     #Probably not the best way, but I could not find away to guarantee container will be removed
     def __del__(self):
@@ -109,20 +109,20 @@ class BaseRuntimeSmokeTest(Test):
             test_ret, test_output = run_cmd.run_docker_cmd(test, rm_container=rm_container)
             if test_ret == 0:
                 self.log.info("smoke test command '%s' succeeded with output:\n%s" %
-                    (test, test_output))
+                              (test, test_output))
             else:
                 self.error("command '%s' returned unexpected exit status %d; output:\n%s" %
-                    (test, test_ret, test_output))
+                           (test, test_ret, test_output))
 
 
         for test in smoke_fail:
             test_ret, test_output = run_cmd.run_docker_cmd(test)
             if test_ret == 0:
                 self.error("smoke test command '%s' succeeded unexpectly with output:\n%s" %
-                    (test, test_output))
+                           (test, test_output))
             else:
                 self.log.info("command '%s' returned exit status %d; output:\n%s" %
-                    (test, test_ret, test_output))
+                              (test, test_ret, test_output))
 
 
     def _check_cmd_result(self, cmd, return_code, cmd_output, expect_pass=True):
@@ -181,12 +181,12 @@ class BaseRuntimeSmokeTest(Test):
         #    error = True
 
         #Test new user functionality
-        cmd = "su - usertest; touch ~/testfile.txt"
+        cmd = "su - usertest && touch ~/testfile.txt"
         test_ret, test_output = run_cmd.run_docker_cmd(cmd, rm_container=False)
         if not self._check_cmd_result(cmd, test_ret, test_output):
             error = True
 
-        cmd = "su - usertest; ls -allh ~/testfile.txt"
+        cmd = "su - usertest && ls -allh ~/testfile.txt"
         test_ret, test_output = run_cmd.run_docker_cmd(cmd, rm_container=False)
         if not self._check_cmd_result(cmd, test_ret, test_output):
             error = True
